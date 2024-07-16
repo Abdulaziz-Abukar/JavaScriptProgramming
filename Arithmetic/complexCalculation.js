e4be163f991d0c2993e5dca416672e9e0f8f7892
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-16 13:25:03 -0600

    Arithmetics

---
+++ Arithmetic/complexCalculation.js
@@ -0,0 +1,7 @@
+var x = 6;
+var y = 3;
+var z = 2;
+
+var result = ((x + y) * z) / y - z;
+
+console.log(result)
---
+++ Arithmetic/modulusOperation.js
@@ -0,0 +1,5 @@
+var num = 29;
+
+var remainder = num % 5;
+
+console.log(remainder);
