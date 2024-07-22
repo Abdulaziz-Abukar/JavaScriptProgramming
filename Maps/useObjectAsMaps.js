689ef2c303cf8c8fb149753b0e868e706f81ebf7
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-22 12:26:07 -0600

    .

---
+++ Maps/useObjectAsMaps.js
@@ -0,0 +1,16 @@
+/*
+Use Object as a Map
+
+Create an object named course with properties title, instructor, and duration. Add a new property level and log the updated object to the console.
+*/
+
+let course = {
+    title: 'JavaScript: The Advanced Concepts',
+    instructor: 'Colt Steele',
+    duration: '8 weeks'
+}
+
+course.teacherAssistant = "Abdulaziz";
+
+console.log(course.teacherAssistant);
+console.log(course.title);
