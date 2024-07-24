c58ad4c73d4d90bc36d7b568383d2c80b9de6bcf
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-24 13:51:23 -0600

    power and square root of numbers

---
+++ Math Object/powerAndSquareRoot.js
@@ -0,0 +1,14 @@
+/*
+Power and Square Root
+
+Calculate the value of 3 to the power of 4 using Math.pow and the square root of 49 using Math.sqrt. Log the results to the console.
+*/
+
+let x = Math.pow(3, 4);
+
+console.log(x);
+
+let y = Math.sqrt(48);
+
+console.log(Math.round(y));
+
