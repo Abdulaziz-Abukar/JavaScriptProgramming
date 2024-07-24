c2c89d05e83ecec85c60fb2f3ded5b6454b142b1
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-24 13:25:14 -0600

    Getting date components

---
+++ Date Object/getDateComponents.js
@@ -0,0 +1,14 @@
+/*
+Get Date Components
+
+Create a Date object for the current date. Log the year, month, day, hours, minutes, and seconds separately to the console.
+*/
+
+let theDate = new Date();
+
+console.log(theDate.getFullYear());
+console.log(theDate.getMonth() + 1);
+console.log(theDate.getDate());
+console.log(theDate.getHours());
+console.log(theDate.getMinutes());
+console.log(theDate.getSeconds());
