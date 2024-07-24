08e4345fc545c7517ad11bdf3a4b0165f4929fd7
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-24 13:28:03 -0600

    setting Date Components.

---
+++ Date Object/setDateComponents.js
@@ -0,0 +1,16 @@
+/*
+Set Date Components
+
+Create a Date object for the current date. Change the year to 2023, the month to January, and the day to 1. Log the updated date to the console.
+*/
+
+let theDate = new Date();
+
+theDate.setFullYear(2023);
+
+theDate.setMonth(0);
+
+theDate.setDate(1);
+
+
+console.log(theDate);
