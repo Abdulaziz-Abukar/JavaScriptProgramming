e149f817ec4492f983cd568e697ca4989c77fde4
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-24 00:06:09 -0600

    file made for testing template literals

---
+++ String Object/templateLiterals.js
@@ -0,0 +1,18 @@
+/*
+Template Literals
+
+Create variables firstName and lastName with your first and last name. 
+Use a template literal to create a string that says "Hello, [firstName] [lastName]! Welcome to JavaScript." Log the result to the console. 
+Also, create a multi-line string using template literals that lists three of your favorite hobbies.
+*/
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(`Hello, ${firstName} ${lastName}! Welcome to JavaScript.`)
+
+console.log(`My Hobbies consist of:
+    1. Coding
+    2. Programming
+    3. Reading`)
+
