543e952e7df5016e9804b14523a1ae598c4b178d
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-23 23:59:34 -0600

    adds a file for splitting strings in js

---
+++ String Object/splittingString.js
@@ -0,0 +1,11 @@
+/*
+Splitting a String
+
+Create a string named colors with the value "red,green,blue,yellow". Use the split method to convert the string into an array of colors and log the result to the console.
+*/
+
+let colors = "red,green,blue,yellow";
+
+let words = colors.split(",");
+
+console.log(words);
