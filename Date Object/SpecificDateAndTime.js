57536b8ec62b25551a2b85ec8f672c53c53ceb0c
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-24 13:23:21 -0600

    specific date

---
+++ Date Object/SpecificDateAndTime.js
@@ -0,0 +1,9 @@
+/*
+Specific Date and Time
+
+Create a Date object for your birthday (year, month, day, hour, minute, second). Log the date to the console.
+*/
+
+let birthday = new Date("2000-01-27T00:00:00");
+
+console.log(birthday);
