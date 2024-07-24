0a104efea17b2b68ebb022282fea03d23fbb88af
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-23 23:53:27 -0600

    adds a replacing string file.

---
+++ String Object/replacingString.js
@@ -0,0 +1,9 @@
+/*
+Replacing in a String
+
+Create a string named greeting with the value "Good morning, world!". Use the replace method to change "morning" to "evening" and log the result to the console.
+*/
+
+let greeting = "Good morning, world!";
+greeting = greeting.replace("morning", "evening");
+console.log(greeting);
