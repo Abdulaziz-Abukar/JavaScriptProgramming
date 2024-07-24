d2140e818ae27de43561e539c0f712407b1c8e24
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-24 13:48:30 -0600

    finding max num

---
+++ Math Object/findingMaxandMin.js
@@ -0,0 +1,8 @@
+/*
+Finding Maximum and Minimum
+
+Create an array of numbers [10, 20, 5, 15, 30]. Use the Math.max and Math.min methods to find the largest and smallest numbers in the array. Log the results to the console.
+*/
+
+let nums = [10, 20, 5, 15, 30];
+// console.log(Math.max(...nums)); // 30
