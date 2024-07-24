49fea02f90f6df173e03b2295247876f341127ea
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2024-07-23 23:56:08 -0600

    adds other string methods file

---
+++ String Object/otherStringMethods.js
@@ -0,0 +1,14 @@
+/*
+Using Other String Methods
+
+Create a string named message with the value " JavaScript is fun! ". Use the toUpperCase, toLowerCase, and trim methods and log the results to the console. 
+Also, use the slice method to extract "JavaScript" from the string and log the result.
+*/
+
+let message = " JavaScript is fun! ";
+
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.trim());
+console.log(message.slice(1, 11));
+
