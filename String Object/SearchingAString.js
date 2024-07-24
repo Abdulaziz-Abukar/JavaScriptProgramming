ebe4bb0da329d90eafd41d86346969efc1735074
Abdulaziz Abukar <aabuk19@wgu.edu>
2024-07-23 23:51:31 -0600

    for searching an element in a string

---
+++ String Object/SearchingAString.js
@@ -0,0 +1,11 @@
+/*
+Searching a String
+
+Create a string named sentence with the value "The quick brown fox jumps over the lazy dog." Use the indexOf, lastIndexOf, and includes methods to search for the word "fox" and log the results to the console.
+*/
+
+let sentence = "The quick brown fox jumps over the lazy dog."
+
+console.log(sentence.indexOf("fox"));
+console.log(sentence.lastIndexOf("fox"));
+console.log(sentence.includes("fox"));
